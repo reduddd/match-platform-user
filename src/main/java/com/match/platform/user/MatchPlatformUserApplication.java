aee633c13f360b60f5b9601e08dd33e813963be6
Dpy <pengyudou@jieyuechina.com>
2018-07-24 18:21:01 +0800

    first commit

---
+++ src/main/java/com/match/platform/user/MatchPlatformUserApplication.java
@@ -0,0 +1,12 @@
+package com.match.platform.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchPlatformUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchPlatformUserApplication.class, args);
+	}
+}
